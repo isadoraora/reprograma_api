341921fc5bd4ad5b5fe490a13a442ce10ed28fae
isadoraora <isa.mdeo@gmail.com>
2019-10-11 22:02:13 -0300

    última aula de API com a Nat

--- src/app.js
+++ src/app.js
@@ -6,10 +6,14 @@ const index = require('./routes/index')
 const alunas = require('./routes/alunasRoute')
 const professoras = require('./routes/professorasRoute')
 
-app.all('*', function (req, res,next) {
-    console.log('passamu pelo app, manooo!')
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
     next()
 })
+
 
 app.use("/", index)
 app.use("/alunas", alunas)
--- src/controllers/professorasController.js
+++ src/controllers/professorasController.js
@@ -1,7 +1,61 @@
 const professoras = require("../model/professoras.json")
-
-
-exports.get = (req, res)=>{
+exports.get = (req, res) => {
     console.log(req.url)
     res.status(200).send(professoras)
 }
+
+exports.getById = (req, res) => {
+    const id = req.params.id
+    const prof = professoras.find(professoras => professoras.id == id)
+    delete prof.cpf
+    res.status(200).send(prof)
+}
+// exports.get = (req, res) => {
+//     const novaArr = []
+//     for (let i = 0; i < professoras.length; i++) {
+//         const semCpf = {}
+//         semCpf.id = professoras[i].id
+//         semCpf.nome = professoras[i].nome
+//         semCpf.especialidade = professoras[i].especialidade
+//         semCpf.signo = professoras[i].signo
+//         novaArr.push(semCpf)
+//     }
+//     res.status(200).send(novaArr)
+// }
+
+//Codigo da Valentina
+// exports.get = (req, res) => {
+//     const profSemCpf = professoras.map(item => {
+//         item.cpf = '*********'
+//         return item;
+//     })
+//     res.status(200).send(profSemCpf)
+// }
+
+//Codigo da Shirley
+// exports.get = (req, res) => {
+//     const profSemCpf = professoras.map(item => {
+//         delete item.cpf
+//         return item
+//     })
+//     res.status(200).send(profSemCpf)
+// // }
+// exports.getById = (req, res) => {
+//     const id = req.params.id
+//     console.log(id)
+//     const professoras = professoras.find(professoras => professoras.id == id)
+//     const professorasSemCpf = professoras.map(item => {
+//         delete item.cpf
+//         return item
+//     })
+//     res.status(200).send(professorasSemCpf)
+// }
+
+// // exports.getCpf = (req, res) => {
+// //     for (let i = 0; i < professoras.length; i++) {
+//         const professorasSemCpf = professoras.map(function (cpf) {
+//             professorasSemCpf.push(professoras[i] - 1)
+//             console.log(professorasSemCpf)
+//         })
+//     }
+// }
